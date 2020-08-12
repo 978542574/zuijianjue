a37cc75e3497cf1ade1ca2ce239d863355e842e7
gitdu <978542574@qq.com>
2020-08-12 16:47:58 +0800

    tes2修改3

--- src/main/java/com/java/dao/testdao.java
+++ src/main/java/com/java/dao/testdao.java
@@ -1,7 +1,12 @@
 package com.java.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class testdao {
     public static void main(String[] args) {
         System.out.println("ss");
+        List<String> l=new ArrayList<>();
+        l.add("apper");
     }
 }
