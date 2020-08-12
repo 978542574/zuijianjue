6168446078129feb9f166ab566953f197a952f33
gitdu <978542574@qq.com>
2020-08-12 17:28:39 +0800

    tes2修改3

--- src/main/java/com/java/dao/testdao.java
+++ src/main/java/com/java/dao/testdao.java
@@ -1,14 +1,17 @@
 package com.java.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class testdao {
     public static void main(String[] args) {
         System.out.println("ss");
         System.out.println("again");
-        System.out.println("7777777777");
         List<String> l=new ArrayList<>();
         l.add("apper");
+        Map<String,String> mp=new HashMap<>();
+        System.out.println("7777777777");
     }
 }
