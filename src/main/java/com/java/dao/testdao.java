b31a435ae4556c92add13c9129f8f042f6ca42bd
gitdu <978542574@qq.com>
2020-08-12 17:26:11 +0800

    tes2修改3

--- src/main/java/com/java/dao/testdao.java
+++ src/main/java/com/java/dao/testdao.java
@@ -7,7 +7,6 @@ public class testdao {
     public static void main(String[] args) {
         System.out.println("ss");
         System.out.println("again");
-        System.out.println("3");
         List<String> l=new ArrayList<>();
         l.add("apper");
     }
