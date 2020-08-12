3126093978098bae7bef9606de5bbb3335848096
gitdu <978542574@qq.com>
2020-08-12 18:01:38 +0800

    试试

--- src/main/java/com/java/dao/testdao.java
+++ src/main/java/com/java/dao/testdao.java
@@ -1,14 +1,15 @@
 package com.java.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class testdao {
     public static void main(String[] args) {
+        System.out.println("只要自己的99999");
         List<String> l=new ArrayList<>();
         l.add("apper");
-        System.out.println("只要自己的");
-        System.out.println("只要自己的2");
-        System.out.println("只要自己的3");
+        Map<String,String> mp=new HashMap<>();
     }
 }
